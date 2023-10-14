deb26b93fe15cac5d4e760b15001b550d466d16b
Korotkov2 <147937893+Korotkov2@users.noreply.github.com>
2023-10-14 19:30:41 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task2.py
@@ -0,0 +1,10 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+
+symv= 4
+chars= 25
+string_= 50
+page = 100
+book=symv*chars*string_*page
+book=book/(1024*1024)
+books=1.44//book
+print("Количество книг, помещающихся на дискету:", round(books))
---
+++ Лабораторная работа 1/task3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+teams=len(list_players)//2
+first= list_players[:teams]
+second = list_players[teams:]
+print(first)
+print(second)
+# TODO Разделите участников на две команды
